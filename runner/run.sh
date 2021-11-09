fcac1a9aaa6817f1324b239cdeb71d6cc61b2322
dmaley <dmaley@appannie.com>
2021-11-10 00:41:36 +0100

    launch after everything is up

--- runner/run.sh
+++ runner/run.sh
@@ -5,5 +5,9 @@ until nc -z ${RABBIT_HOST} ${RABBIT_PORT}; do
     sleep 1
 done
 
+sleep 5
+
+echo "$(date) - Ready to launch the stream"
+
 python runner.py
 echo "$(date) - The streaming process has started"
--- runner/runner.py
+++ runner/runner.py
@@ -1,5 +1,6 @@
 from envyaml import EnvYAML
 from nameko.standalone.rpc import ServiceRpcProxy
+from nameko.exceptions import UnknownService
 
 config = EnvYAML('config.yml')
 
@@ -9,5 +10,8 @@ SPEED = '1000ms'
 LIMIT = 1000
 
 with ServiceRpcProxy('listener', config) as proxy:
-    proxy.get_order_book(CC, LIMIT)
-    proxy.start_stream(CC, STREAM, SPEED)
+    try:
+        proxy.get_order_book(CC, LIMIT)
+        proxy.start_stream(CC, STREAM, SPEED)
+    except UnknownService as ex:
+        print('Listener was not ready')
